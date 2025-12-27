4e1f19c59a6f6595d4aa61238882d8b215e5ac54
bharath7654321 <mramamoorthi556@gmail.com>
2025-12-27 21:57:32 +0530

    Delete College-project directory

--- College-project/FE/student-attendance/src/app/guards/auth.guard.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-import { inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-
-export const authGuard: CanActivateFn = () => {
-  return inject(AuthService).isTeacherLoggedIn();
-};
